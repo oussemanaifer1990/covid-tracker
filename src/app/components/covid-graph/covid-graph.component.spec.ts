661f86f268d395376a92c04fe5f481ded6ee2913
oussema.naifer <oussema.naifer@itserv.tn>
2022-01-12 18:17:10 +0100

    first commit

--- src/app/components/covid-graph/covid-graph.component.spec.ts
+++ src/app/components/covid-graph/covid-graph.component.spec.ts
@@ -1,12 +1,16 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { CovidGraphComponent } from './covid-graph.component';
+import {HttpClientTestingModule} from "@angular/common/http/testing";
 
 describe('CovidGraphComponent', () => {
   let component: CovidGraphComponent;
   let fixture: ComponentFixture<CovidGraphComponent>;
 
   beforeEach(async () => {
+    TestBed.configureTestingModule({
+      imports: [ HttpClientTestingModule ]
+    });
     await TestBed.configureTestingModule({
       declarations: [ CovidGraphComponent ]
     })
